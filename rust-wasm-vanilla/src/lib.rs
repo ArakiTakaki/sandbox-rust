53c0824154f6c3588cda2a4c186a6678f6f34a94
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-07-30 02:32:08 +0900

    🚀 : 一旦コミット

--- rust-wasm-vanilla/src/lib.rs
+++ rust-wasm-vanilla/src/lib.rs
@@ -1,36 +1,19 @@
-#[no_mangle]
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+/**
+ * 自身に定義する関数
+ */
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
 }
 
-#[no_mangle]
-pub fn fib(count: u32) -> u32 {
-    if count < 2 {
-        return count;
-    }
-
-    let (_, ret) = (0..count-2).fold((0, 1), |(former, ret), _| (ret, ret + former));
-    ret
+/**
+ * 外に渡す関数
+ */
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("hello, {}!", name));
 }
-
-#[no_mangle]
-pub fn fib_array(count: u32) -> Vec<u32> {
-    match count {
-        0 => vec![],
-        1 => vec![0],
-        2 => vec![0, 1],
-        _ => (2..count).fold(vec![0, 1], |mut acc, i| {
-            let new = acc[(i-1) as usize] + acc[(i-2) as usize];
-            acc.push(new);
-            acc
-        })
-    }
-}
-
-fn main() {
-    println!("{:?}", add(10, -100));
-    println!("{:?}", fib(40));
-    println!("{:?}", fib_array(10));
-    assert!(fib_array(10).len() == 10);
-}
-
