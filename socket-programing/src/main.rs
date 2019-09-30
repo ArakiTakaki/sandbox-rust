90dfd328d766b697dc0223b70401782c5fe5bfb3
ArakiTakaki <araki_takaki@yahoo.co.jp>
2019-10-01 02:07:24 +0900

    🚀 : http socket 入門

--- socket-programing/src/main.rs
+++ socket-programing/src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
+#[macro_use]
+extern crate log;
+
+mod tcp_client;
+mod tcp_server;
+mod udp_client;
+mod udp_server;
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        error!("Pleasespecify[tcp|udp][server|client][addr:port].");
+        std::process::exit(1);
+    }
+    let protocol: &str = &args[1];
+    let role: &str = &args[2];
+    let address = &args[3];
+    println!("{}", protocol);
+    println!("{}", role);
+    println!("{}", address);
 }
